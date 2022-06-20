2b82df7d7a7c081a8ce0075d6e16d82734325bf8
Henrique Hott <dev.henriquehott@gmail.com>
2022-06-20 12:55:30 -0300

    KMP: Stack implemented.

--- src/kmp.c
+++ src/kmp.c
@@ -3,7 +3,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct note_stack_node_t {
+	struct note_stack_node_t * previous;
+	note n;
+} * NSNode;
+
+typedef struct note_stack_t {
+	NSNode top;
+	int sz;
+} * NStack;
+
+NStack ns_New(void);
+note ns_Pop(NStack s);
+void ns_Add(NStack s, note n);
+void ns_Free(NStack s);
+
 void KMP(note * original, int M, note * suspect, int T) {
-	fprintf(stderr, "ERROR: %s not implemented yet.\n", __func__);
-	exit(EXIT_FAILURE);
+	NStack s = ns_New();
+
+
+	ns_Free(s);
+}
+
+NStack ns_New(void) {
+	NStack s = (NStack) malloc(sizeof(struct note_stack_t));
+	s->sz = 0;
+	return s;
+}
+
+note ns_Pop(NStack s) {
+	if (s->sz == 0) {
+		fputs("ERROR: Trying to pop a note from an empty stack.\n", stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	NSNode popped = s->top;
+	NSNode newTop = popped->previous;
+	
+	note result = popped->n;
+
+	s->sz--;
+	s->top = newTop;
+	free(popped);
+
+	return result;
+}
+
+void ns_Add(NStack s, note n) {
+	NSNode new = (NSNode) malloc(sizeof(struct note_stack_node_t));
+	new->n = n;
+
+	new->previous = s->top;
+	s->top = new;
+
+	s->sz++;
+}
+
+void ns_Free(NStack s) {
+	while (s->sz > 0) {
+		ns_Pop(s);
+	}
+	free(s);
 }
